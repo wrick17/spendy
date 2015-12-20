30694d80e5d0256973b0edca76b8abebb1cfcc16
Pratyush Poddar <pratyush.poddar@compassitesinc.com>
2015-12-20 22:58:18 +0530

    initiate start point

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+    path = require('path'),
+    superagent = require('superagent'),
+    app = express();
+
+app.use(express.static(path.join('./')));
+
+app.get('/', function(request, response) {
+  console.log('client');
+  superagent
+    .get('http://localhost:3001')
+    .end(function(req, res) {
+      response.send(res.text);
+    });
+});
+
+var server = app.listen(process.env.PORT || 3000, function() {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('client server started at port ', port);
+
+});
