4f6ff4ca601dd9819c7d338bc7bacb2e8305eb1f
Pratyush Poddar <pratyush.poddar@compassitesinc.com>
2016-01-06 17:11:46 +0530

    proper http enforcing

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ var express = require('express'),
 app.use(express.static(path.join('./')));
 
 app.get('/*', function(req, res) {
- res.sendFile(__dirname + '/index.html');
+  if (req.hostname !== 'localhost' && req.protocol === 'http') {
+    res.writeHead(301, {
+      Location: "https://" + req.headers["host"] + req.url
+    });
+    res.end();
+  }
+  res.sendFile(__dirname + '/entry.html');
 });
 
 var server = app.listen(process.env.PORT || 3000, function() {
