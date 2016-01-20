c990f1b2597d6a7d614a694725b68824588fc40d
Pratyush Poddar <pratyush.poddar@compassitesinc.com>
2016-01-20 16:09:18 +0530

    add nodemon to the gulp process also

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var less       = require('gulp-less');
 var watch      = require('gulp-watch');
 var webpack    = require('gulp-webpack');
 var notify     = require("gulp-notify");
+var nodemon    = require("gulp-nodemon");
 
 
 /* Task to compile less */
@@ -29,5 +30,11 @@ gulp.task('watch-jsx', function() {
   gulp.watch('./app/**/*.js*' , ['webpack']);
 });
 
+gulp.task('nodemon', function () {
+  nodemon({
+    script: 'server.js'
+  })
+});
+
 /* Task when running `gulp` from terminal */
-gulp.task('default', ['webpack', 'compile-less', 'watch-jsx', 'watch-less']);
+gulp.task('default', ['webpack', 'compile-less', 'nodemon', 'watch-jsx', 'watch-less']);
